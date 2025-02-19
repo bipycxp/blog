dfac07761dad27bbb73831e12477d1e802133b95
Andrew Klymets <bipycxp@gmail.com>
2025-02-19 23:19:31 +0200

    Fix links

--- packages/blog-starter-kit/themes/enterprise/components/footer.tsx
+++ packages/blog-starter-kit/themes/enterprise/components/footer.tsx
@@ -11,16 +11,16 @@ export const Footer = () => {
 			<Container>
 				<div className="flex flex-col sm:flex-row items-center justify-between">
 					<div className="text-white mb-4 sm:mb-0 font-mulish font-bold">
-						<Link href="/">
+						<Link href="https://neary.io">
 							<Image className="md:max-w-none mx-auto inline mr-4" src={PUBLICATION_LOGO ?? ''} width={70} height={51} alt="Neary.io Logo" />
 							<span>Neary.io</span>
 						</Link>
 					</div>
 					<div>
 						<div className="text-sm font-light text-white space-x-4">
-							<Link href="/faq">FAQ</Link>
-							<Link href="/policies/terms-of-use">Terms of use</Link>
-							<Link href="/policies/privacy">Privacy policy</Link>
+							<Link href="https://neary.io/faq">FAQ</Link>
+							<Link href="https://neary.io/policies/terms-of-use">Terms of use</Link>
+							<Link href="https://neary.io/policies/privacy">Privacy policy</Link>
 						</div>
 					</div>
 				</div>
